774f9e4b1166f67a8cef824d3471565adf35a871
Tim Shadel <tim@daylogger.com>
2019-11-13 23:26:14 -0700

    fix: package setup for Xcode

--- Package.swift
+++ Package.swift
@@ -1,12 +1,23 @@
+// swift-tools-version:5.0
 /*
  |  _   ____   ____   _
- | | |‾|  ⚈ |-| ⚈  |‾| |
- | | |  ‾‾‾‾| |‾‾‾‾  | |
+ | ⎛ |‾|  ⚈ |-| ⚈  |‾| ⎞
+ | ⎝ |  ‾‾‾‾| |‾‾‾‾  | ⎠
  |  ‾        ‾        ‾
  */
 
 import PackageDescription
 
 let package = Package(
-    name: "DeviceInfo"
+    name: "DeviceInfo",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "DeviceInfo", targets: ["DeviceInfo"])
+    ],
+    targets: [
+        .target(name: "DeviceInfo", path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
 )
